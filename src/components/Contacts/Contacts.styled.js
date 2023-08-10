7717ee353607aeef34dc852d1b1783b4c112c6e0
Natalia0310 <ndemydenko@ukr.net>
2023-08-10 16:26:44 +0300

    update3

--- src/components/Contacts/Contacts.styled.js
+++ src/components/Contacts/Contacts.styled.js
@@ -1,28 +1,10 @@
 import styled from 'styled-components';
 
 export const Table = styled.table`
-  margin: 20px auto;
-  width: 400px;
+padding: 20px;
 
-  & td {
-    padding: 10px;
-  }
+margin: var(--default-margin);
+padding: 2px;
 
-  & td:first-child {
-    font-weight: bold;
-  }
-
-  & button {
-    padding: 5px 10px;
-    background-color: #a8dadc;
-    border: 0px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: transform 200ms ease-in-out, background-color 200ms ease-in;
-  }
-
-  & button:hover {
-    background-color: #457b9d;
-    transform: scale(1.05);
-  }
+background-image: linear-gradient(30deg, var(--dark-clr-2), var(--light-clr-2), var(--dark-clr-3));
 `;
--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 export const Title = styled.h1`
+`;
  
 
-  margin: var(--default-margin);
-  padding: 2px;
+  
 
-  background-image: linear-gradient(30deg, var(--dark-clr-2), var(--light-clr-2), var(--dark-clr-3));
-`;
